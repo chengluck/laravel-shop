4537422448f88686d20809c29aca5a414eebc2b2
chengluck <wangcheng_luck@163.com>
2020-08-29 17:16:55 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,7 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
 
     // 购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
 });
